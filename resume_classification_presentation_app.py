97bfd8559cba53ad461ffd2533a2446ab928b484
Adith Kadadi <adith2015@gmail.com>
2025-07-20 16:46:22 +0530

    Update resume_classification_presentation_app.py

--- resume_classification_presentation_app.py
+++ resume_classification_presentation_app.py
@@ -38,16 +38,19 @@ import nltk
 import warnings
 from nltk.corpus import stopwords
 
-nltk.data.path.append("C:/Users/Adith/AppData/Roaming/nltk_data")
 nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
+os.makedirs(nltk_data_path, exist_ok=True)
 nltk.data.path.append(nltk_data_path)
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('vader_lexicon')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('movie_reviews')
-nltk.download('conll2000')
-nltk.download('brown')
+
+# Download all required resources into that folder
+nltk.download('stopwords', download_dir=nltk_data_path)
+nltk.download('wordnet', download_dir=nltk_data_path)
+nltk.download('vader_lexicon', download_dir=nltk_data_path)
+nltk.download('averaged_perceptron_tagger', download_dir=nltk_data_path)
+nltk.download('movie_reviews', download_dir=nltk_data_path)
+nltk.download('conll2000', download_dir=nltk_data_path)
+nltk.download('brown', download_dir=nltk_data_path)
+nltk.download('punkt', download_dir=nltk_data_path)
 my_stop_words = set(stopwords.words("english"))
 warnings.filterwarnings('ignore', message='X does not have valid feature names')
 
